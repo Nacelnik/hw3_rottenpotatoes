c22e70fc127ed0318b1eae560848ad1cc3fbf2aa
Petr Vymola <p.vymola@gmail.com>
2012-03-07 12:52:49 +0100

    ordering 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,8 +10,10 @@ class MoviesController < ApplicationController
     if (params.has_key?(:sort_param))
       if (params[:sort_param] == "title")
         @movies = Movie.all(:order => "title ASC")
+        flash[:notice] = "Sort by title"
       elsif (params[:sort_param] == "date")
         @movies = Movie.all(:order => "release_date ASC")
+        flash[:notice] = "Sort by date"
       end  
     end
     @movies = Movie.all
