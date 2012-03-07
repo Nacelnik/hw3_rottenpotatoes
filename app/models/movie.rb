0e994e8fa0ccf01a30b52d38351053b316a8e667
Petr Vymola <p.vymola@gmail.com>
2012-03-07 14:33:17 +0100

    oprava fatal erroru

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ActiveRecord::Base
+  @@possible_ratings = ['G','PG','PG-13','R','NC-17']
 
-  def @@getPossibleRatings()
-    ['G','PG','PG-13','R','NC-17']
+  def getPossibleRatings()
+    @@possible_ratings
   end
 
 end
