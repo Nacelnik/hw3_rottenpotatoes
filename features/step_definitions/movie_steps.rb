63e5ab92273dd086758e59321cbe9f1e9dd9d403
saasbook <saasbook@saasbook.(none)>
2012-03-23 02:26:20 -0700

    hw4 snad hotovo

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -19,7 +19,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|  
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  assert false, "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
